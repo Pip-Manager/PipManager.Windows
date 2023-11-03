9df869221b4ef433ba6cd8fceaee7cc8452ce90f
AuroraZiling <2935876049@qq.com>
2023-11-03 21:38:36 +0800

    build: build.ps1

---
+++ build.ps1
@@ -0,0 +1,26 @@
+param(
+    [string] $Architecture = "x64",
+    [string] $Version = "1.0.0.1"
+)
+
+$ErrorActionPreference = "Stop";
+
+Write-Output "Start building singleWithRuntime...";
+
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=true -p:SelfContained=true -p:AssemblyVersion=$Version;
+
+Write-Output "Start building singleWithoutRuntime...";
+
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=true -p:SelfContained=false -p:AssemblyVersion=$Version;
+
+Write-Output "Start building extractedWithRuntime...";
+
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/extractedWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=false -p:SelfContained=true -p:AssemblyVersion=$Version;
+
+Write-Output "Start building extractedWithoutRuntime...";
+
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/extractedWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=false -p:SelfContained=false -p:AssemblyVersion=$Version;
+
+Write-Output "Build Finished";
+
+[Console]::ReadKey()
