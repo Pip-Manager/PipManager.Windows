bcd8c64e265fab83801a69572d637515d6b94bba
AuroraZiling <2935876049@qq.com>
2023-11-18 18:33:51 +0800

    build: remove redundant

--- build.ps1
+++ build.ps1
@@ -7,19 +7,19 @@ $ErrorActionPreference = "Stop";
 
 Write-Output "Start building singleWithRuntime...";
 
-dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=true -p:SelfContained=true -p:AssemblyVersion=$Version;
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:EnableCompressionInSingleFile=true -p:PublishSingleFile=true -p:SelfContained=true -p:AssemblyVersion=$Version;
 
 Write-Output "Start building singleWithoutRuntime...";
 
-dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=true -p:SelfContained=false -p:AssemblyVersion=$Version;
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:EnableCompressionInSingleFile=false -p:PublishSingleFile=true -p:SelfContained=false -p:AssemblyVersion=$Version;
 
 Write-Output "Start building extractedWithRuntime...";
 
-dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/extractedWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=false -p:SelfContained=true -p:AssemblyVersion=$Version;
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/extractedWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:EnableCompressionInSingleFile=false -p:PublishSingleFile=false -p:SelfContained=true -p:AssemblyVersion=$Version;
 
 Write-Output "Start building extractedWithoutRuntime...";
 
-dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/extractedWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false -p:TieredPGO=true -p:TieredCompilationQuickJitForLoops=true -p:PublishSingleFile=false -p:SelfContained=false -p:AssemblyVersion=$Version;
+dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/extractedWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:EnableCompressionInSingleFile=false -p:PublishSingleFile=false -p:SelfContained=false -p:AssemblyVersion=$Version;
 
 Write-Output "Build Finished";
 
