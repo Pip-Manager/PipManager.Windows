52464479c79a170fd6e34d7b996a2529aa8ce2fe
AuroraZiling <2935876049@qq.com>
2024-01-31 17:37:19 +0800

    add: github issue template

--- build.ps1
+++ build.ps1
@@ -9,6 +9,8 @@ Write-Output "Start building singleWithRuntime...";
 
 dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:EnableCompressionInSingleFile=true -p:PublishSingleFile=true -p:SelfContained=true -p:AssemblyVersion=$Version;
 
+Rename-Item -Path "build/$Version/singleWithRuntime/PipManager.exe" -NewName "PipManager_withRuntime.exe"
+
 Write-Output "Start building singleWithoutRuntime...";
 
 dotnet publish src/PipManager -c Release -r "win-$Architecture" -o "build/$Version/singleWithoutRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=false -p:EnableCompressionInSingleFile=false -p:PublishSingleFile=true -p:SelfContained=false -p:AssemblyVersion=$Version;
