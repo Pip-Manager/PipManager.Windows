b7755b79245f7f3e3b3b488df5ebd301949b7d1c
Mccree Lee <2935876049@qq.com>
2024-04-07 16:10:31 +0800

    perf: caught exceptions in log&cache&crushes

--- src/PipManager/ViewModels/Pages/Settings/SettingsViewModel.cs
+++ src/PipManager/ViewModels/Pages/Settings/SettingsViewModel.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Net.Http;
+using PipManager.Views.Windows;
 using Wpf.Ui;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
